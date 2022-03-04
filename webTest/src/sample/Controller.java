109d90f8fda4e8bb3aed3c26b984399b9d929f25
lingduliudu <lingduliudu@163.com>
2022-03-04 17:37:17 +0800

    loading

--- webTest/src/sample/Controller.java
+++ webTest/src/sample/Controller.java
@@ -13,11 +13,14 @@ public class Controller extends Application {
     public void start(Stage stage) throws Exception {
         try {
             stage.setTitle("测试");
+            stage.initStyle(StageStyle.UNDECORATED);
             WebView w = new WebView();
             WebEngine e = w.getEngine();
-            e.load("http://localhost:10001/index.html");
-            Scene scene = new Scene(w, w.getPrefWidth(),
-                    w.getPrefHeight());
+            //e.load("http://localhost:10001/index.html");
+            e.loadContent("<body style='backgroud-color:red;height:30px;'>HELLO</bod>");
+           /* Scene scene = new Scene(w, w.getPrefWidth(),
+                    w.getPrefHeight());*/
+            Scene scene = new Scene(w, 300, 30);
             stage.setScene(scene);
             // 设置工具栏
             ToolBar toolBar = new ToolBar(w);
@@ -27,7 +30,8 @@ public class Controller extends Application {
             System.out.println(e.getMessage());
         }
     }
+
     public static void main(String[] args) {
-        launch(Controller.class,args);
+        launch(Controller.class, args);
     }
 }
